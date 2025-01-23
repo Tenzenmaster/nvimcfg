766cbde8518206c1ede38b58f7b4ab0a74ea046e
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2025-01-23 10:59:16 -0500

    stuff

--- init.lua
+++ init.lua
@@ -15,6 +15,10 @@ vim.opt.splitbelow = true
 vim.opt.cursorline = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.gdefault = true
+vim.opt.inccommand = "split"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.signcolumn = "yes"
 
@@ -40,12 +44,40 @@ require("lazy").setup({
     checker = { enable = false },
     spec = {
         {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    ensure_installed = {
+                        "bash",
+                        "c",
+                        "cmake",
+                        "cpp",
+                        "java",
+                        "lua",
+                        "luadoc",
+                        "make",
+                        "python",
+                        "rust",
+                        "toml",
+                        "vim",
+                        "vimdoc",
+                    },
+                    sync_install = false,
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                })
+            end,
+        },
+        {
             "nvim-telescope/telescope.nvim",
             dependencies = { "nvim-lua/plenary.nvim" },
             config = function()
                 local builtin = require("telescope.builtin")
                 vim.keymap.set("n", "<leader>f", builtin.find_files)
                 vim.keymap.set("n", "<leader>b", builtin.buffers)
+                vim.keymap.set("n", "<leader>n", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end)
+                vim.keymap.set("n", "<leader>g", builtin.live_grep)
             end,
         },
         {
